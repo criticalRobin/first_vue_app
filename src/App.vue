0e19b1e05f8496ffd18c6eef974d22f31539dacd
matite <matiastite1@gmail.com>
2024-01-30 18:38:10 -0500

    Created the structure of my first component andl also example of how to import it

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Counter />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from "./components/Counter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Counter,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Counter</h2>
+  <p>10</p>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+};
+</script>
+
+<style></style>
