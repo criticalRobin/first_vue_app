fe58f9a4302c8c7abb8d738b93dc8497a135d9ac
matite <matiastite1@gmail.com>
2024-01-30 19:16:07 -0500

    Example of how to use computed properties

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,22 @@
 <template>
   <h2>Counter</h2>
-  <p>10</p>
+  <p>{{ counter }} <sup>2</sup> = {{ getSquareValue }}</p>
 </template>
 
 <script>
 export default {
   name: "Counter",
+  data() {
+    return {
+      counter: 5,
+    };
+  },
+  // similares a methods, pero computed se quedan en la cache
+  computed: {
+    getSquareValue() {
+      return Math.pow(this.counter, 2);
+    },
+  },
 };
 </script>
 
