2270d805ad5e0d0c042bc8901b046667cb30573a
matite <matiastite1@gmail.com>
2024-02-01 17:47:41 -0500

    Implemented increment and decrement buttons and logic

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,10 @@
 <template>
   <h2>Counter</h2>
   <p>{{ counter }} <sup>2</sup> = {{ getSquareValue }}</p>
+  <div class="buttons">
+    <button @click="increment()">+1</button>
+    <button @click="decrement()">-1</button>
+  </div>
 </template>
 
 <script>
@@ -17,7 +21,33 @@ export default {
       return Math.pow(this.counter, 2);
     },
   },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      if (this.counter === 0) return;
+      this.counter--;
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+.buttons > button {
+  background-color: #64bb87;
+  border-radius: 5px;
+  border: 1px solid white;
+  cursor: pointer;
+  padding: 5px 15px;
+  color: white;
+  font-weight: bold;
+  margin: 0px 5px;
+  transition: 0.3s ease-in-out;
+}
+
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
+</style>
