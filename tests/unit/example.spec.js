cdab997ca71eba516b8038415276836671b9f08c
matite <matiastite1@gmail.com>
2024-02-07 19:06:50 -0500

    Example of asserts with jest expect and their methods

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,8 @@
 describe("Example Component Test", () => {
   test("Number greater than 10", () => {
     let value = 5;
-    value += 8;
+    value += 4;
 
-    if (value > 10) {
-      //ToDo assert
-    } else {
-      throw `${value} is not greater than 10`;
-    }
+    expect(value).toBeGreaterThan(10);
   });
 });
