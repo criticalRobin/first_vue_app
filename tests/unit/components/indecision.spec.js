48d9680ceec9fab2056fc742c9b5b61b3412b228
matite <matiastite1@gmail.com>
2024-02-09 09:54:38 -0500

    Created a mock for our component using vm method of wrapper

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -15,10 +15,13 @@ describe("Pruebas del componente Indecision", () => {
   });
 
   test("Escribir en el input no debe disparar nada (console.log)", async () => {
+    //vm es el objeto que contiene todos los metodos y propiedades del componente
+    const getAnwerSpy = jest.spyOn(wrapper.vm, "getAnswer");
     const input = wrapper.find("input");
     await input.setValue("Hobis Mundo!!!");
 
     expect(clgSpy).toHaveBeenCalledTimes(1);
+    expect(getAnwerSpy).not.toHaveBeenCalled();
   });
 
   test("Escribir el simbolo de '?' debe disparar el fetch", () => {});
